3cf6dd691e7dc1f92d1408313477cea5ebdba543
yorkblansh <yorkblansh@yandex.com>
2023-10-28 21:11:54 +0300

    2 files modified

--- src/shared/hooks/useWideMessage.ts
+++ src/shared/hooks/useWideMessage.ts
@@ -1,6 +1,5 @@
 import { createWideHook } from 'widehook'
 
 export const useWideMessage = createWideHook({
-	initState: 'Click' as 'One Value' | 'Another',
-	mode: 'signal',
+	init: 'Click' as 'One Value' | 'Another',
 })
